b20d404c9aad071584fdb687218e388dea73dc9e
Federico Di Iorio <federico_metallica@hotmail.com>
2024-08-10 14:53:47 -0300

    Se completaron los middleware de filtrado

--- src/middleware/validateParams.middleware.js
+++ src/middleware/validateParams.middleware.js
@@ -1,35 +1,53 @@
 module.exports = {
 
     validateProduct: (req, res, next) => {
-        if (req.params.product === 'puerta' || req.params.product === 'ventana') {
+        const product = req.params.product;
+
+        if (product === 'puerta' || product === 'ventana') {
             return next();
         };
 
-        return res.status(400).json({ message: 'Debe elegir entre "puerta" o "ventana".' });
+        return res.status(404).json({ message: 'Debe elegir entre "puerta" o "ventana".' });
     },
 
     validateOpening: (req, res, next) => {
-        if (req.params.opening === 'exterior' || req.params.opening === 'interior' || req.params.opening === 'corrediza' || req.params.opening === 'fija') {
+        const opening = req.params.opening;
+
+        if (opening === 'exterior' || opening === 'interior' || opening === 'corrediza' || opening === 'fija') {
             return next();
         };
 
-        return res.status(400).json({ message: 'El tipo de abertura seleccionado no existe.' });
+        return res.status(404).json({ message: 'La abertura seleccionada no existe.' });
     },
 
     validateColor: (req, res, next) => {
-        if (req.params.color === 'nogal' || req.params.color === 'roble' || req.params.color === 'liso') {
+        const color = req.params.color;
+
+        if (color === 'nogal' || color === 'roble' || color === 'liso') {
             return next();
         }
 
-        return res.status(400).json({ message: 'El color solicitado no se encuentra disponible' });
+        return res.status(404).json({ message: 'El color solicitado no se encuentra disponible' });
     },
 
     validateOpeningStyle: (req, res, next) => {
-        if (req.params.style === 'banderola' || req.params.style === 'batiente' || req.params.style === 'oscilobatiente' || req.params.style === 'proyectante') {
+        const style = req.params.style;
+
+        if (style === 'banderola' || style === 'batiente' || style === 'oscilobatiente' || style === 'proyectante') {
             return next();
         };
 
-        return res.status(400).json({ message: 'El estilo de abertura seleccionado no existe.' });
+        return res.status(404).json({ message: 'El estilo de abertura seleccionado no existe.' });
     },
+
+    validateOpeningType: (req, res, next) => {
+        const type = req.params.type;
+
+        if (type === 'superpuesta' || type === 'simple' || type === 'lateral' || type === 'lateralSuperpuesta' || type === 'dobleSuperpuesta') {
+            return next();
+        };
+
+        return res.status(404).json({ message: 'El tipo de apertura seleccionado no existe.' });
+    }
 
 };
