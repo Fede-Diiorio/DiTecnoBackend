1c00d80e1f6658864985650573c9e6d36dc49ea8
Federico Di Iorio <federico_metallica@hotmail.com>
2024-08-14 06:36:13 -0300

    Se modificó el modelo de designs

--- src/dao/mongo/models/designs.model.js
+++ src/dao/mongo/models/designs.model.js
@@ -3,7 +3,8 @@ const { Schema } = mongoose;
 
 const DesingSchema = new Schema({
     name: { type: String, required: true },
-    slug: { type: String, required: true }
+    slug: { type: String, required: true },
+    image: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Design', DesingSchema, "designs");
