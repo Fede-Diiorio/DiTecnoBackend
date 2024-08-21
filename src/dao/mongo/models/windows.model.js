c31210b0f179af1fcd9186db12975affe76c877b
Federico Di Iorio <federico_metallica@hotmail.com>
2024-08-21 06:59:31 -0300

    Se modificó el código para permitir imagenes en los estilos de ventana

--- src/dao/mongo/models/windows.model.js
+++ src/dao/mongo/models/windows.model.js
@@ -11,6 +11,7 @@ const TypeSchema = new Schema({
 const StyleSchema = new Schema({
     name: { type: String, required: true },
     slug: { type: String, required: true },
+    image: { type: String, required: true },
     type: [TypeSchema]
 });
 
--- src/dto/styles.dto.js
+++ src/dto/styles.dto.js
@@ -3,6 +3,7 @@ class StylesDTO {
         return data.style.map(s => ({
             name: s.name,
             slug: s.slug,
+            image: s.image,
             id: s._id.toString()
         }));
     };
