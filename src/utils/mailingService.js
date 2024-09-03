8ed6093d2509d61982807b31a649a8b3025f9d98
Federico Di Iorio <federico_metallica@hotmail.com>
2024-09-03 17:46:22 -0300

    Se modificaron los estilos de los emails

--- src/utils/mailingService.js
+++ src/utils/mailingService.js
@@ -20,30 +20,39 @@ class MailingService {
                 const colors = product.colors.map(color => color).join(' - ');
 
                 return `
-                <div style="border-bottom: 1px solid #ddd; padding: 10px; margin-bottom: 10px;">
-                    <h3 style="color: #333;">${product.product} ${product.style}</h3>
-                    <img src="${product.type}" alt="Imagen del ${product.product} ${product.style}" style="max-width: 300px; height: auto;" />
-                    <p style="margin: 5px 0;"><strong>Ancho: </strong>${product.width}</p>
-                    <p style="margin: 5px 0;"><strong>Alto: </strong>${product.height}</p>
-                    <p style="margin: 5px 0;"><strong>Medida de hoja: </strong>${product.casement}</p>
-                    <p style="margin: 5px 0;"><strong>Medida de hoja 2: </strong>${product.casement2}</p>
-                    <p style="margin: 5px 0;"><strong>Vidrio : </strong>DVH ${product.glassType}</p>
-                    <p style="margin: 5px 0;"><strong>Colores requeridos: </strong>${colors}</p>
-                    <p style="margin: 5px 0;"><strong>Cantidad: </strong>${product.quantity}</p>
+                <div style="border: 1px solid #ddd; padding: 10px; margin-bottom: 10px; display: inline-block; width: 25%; vertical-align: top;">
+                    <h3 style="color: #333; text-align: center;">${product.product} ${product.style}</h3>
+                    <div style="display: inline-block; width: 100%;">
+                        <div style="width: 25%; display: inline-block; vertical-align: top;">
+                            <img src="${product.type}" alt="Imagen del ${product.product} ${product.style}" style="max-width: 100%; height: auto;" />
+                        </div>
+                        <div style="width: 58%; display: inline-block; vertical-align: top;">
+                            <p style="margin: 5px 0;"><strong>Ancho: </strong>${product.width}</p>
+                            <p style="margin: 5px 0;"><strong>Alto: </strong>${product.height}</p>
+                            <p style="margin: 5px 0;"><strong>Medida de hoja: </strong>${product.casement}</p>
+                            <p style="margin: 5px 0;"><strong>Medida de hoja 2: </strong>${product.casement2}</p>
+                            <p style="margin: 5px 0;"><strong>Vidrio : </strong>DVH ${product.glassType}</p>
+                            <p style="margin: 5px 0;"><strong>Colores requeridos: </strong>${colors}</p>
+                            <p style="margin: 5px 0;"><strong>Cantidad: </strong>${product.quantity}</p>
+                        </div>
+                    </div>
                 </div>
                 `;
             }).join('');
 
             const htmlContent = `
             <div style="font-family: Arial, sans-serif; color: #444;">
-                <h2 >Solicitud de presupuesto por ${name}</h2>
+                <h2>Solicitud de presupuesto por ${name}</h2>
                 <h3 style="color: #333;">Datos del cliente:</h3>
                 <p style="margin: 5px 0;"><strong>Email: </strong>${email}</p>
                 <p style="margin: 5px 0;"><strong>Teléfono: </strong>${phone}</p>
                 <p style="margin: 5px 0;"><strong>Nombre: </strong>${name}</p>
 
                 <h3 style="color: #333;">Productos solicitados:</h3>
-                ${productDetails}
+                
+                <div style="width: 100%; text-align: center;">
+                    ${productDetails}
+                </div>
             </div>
             `;
 
