91c4a7fa6a42f2906d5021e6617a60e95b423c71
Federico Di Iorio <federico_metallica@hotmail.com>
2024-09-22 18:51:08 -0300

    Modificaciones en doorModel.dto

--- src/dto/doorModel.dto.js
+++ src/dto/doorModel.dto.js
@@ -1,12 +1,10 @@
 export default class DoorModelDTO {
     constructor(door, design) {
+        this.name = door.name;
+        this.slug = door.slug;
         this.id = door._id;
-        this.doorName = door.name;
-        this.doorSlug = door.slug;
-        this.doorImage = door.image;
+        this.image = design.image;
         this.casementQuantity = door.casementQuantity;
-        this.designName = design.name;
-        this.designSlug = design.slug
-        this.designImage = design.image;
+        this.design = design.slug;
     };
 };
